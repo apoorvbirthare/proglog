99938c4a618db2e40a1cf67ecce6a16d041d64ea
Apoorv Birthare <birthare@amazon.com>
2021-12-13 16:54:16 -0800

    Added commit log

---
+++ internal/server/log.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
+
+type Log struct {
+	mu      sync.Mutex
+	records []Record
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+//Appends a record to log
+func (l *Log) Append(record Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	record.Offset = uint64(len(l.records))
+	l.records = append(l.records, record)
+	return record.Offset, nil
+}
+
+func (l *Log) Read(offset uint64) (Record, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	len := uint64(len(l.records))
+	if offset >= len {
+		return Record{}, ErrOffsetNotFound
+	}
+	return l.records[offset], nil
+}
+
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
